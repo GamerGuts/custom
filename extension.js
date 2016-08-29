d9694047858edcffc6e31a328c55c57876e51212
GamerGuts <christie.barger@gmail.com>
2016-08-28 20:35:36 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -17,8 +17,6 @@
         bot.retrieveSettings();
 
         //Extend the bot here, either by calling another function or here directly:
-    var basicBot = {
-        commands: {
             alienCommand = {
                 command: 'alien',
                 rank: 'user',
@@ -35,8 +33,6 @@
                     }
                 }
             };
-        }
-    }
         // You can add more spam words to the bot.
         var spamWords = ['spam1', 'spam2', 'spam3', 'spam4'];
         for (var i = 0; i < spamWords.length; i++) {
