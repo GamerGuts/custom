b8eaa2ec2e2eced378384507f028f1d41373e17f
GamerGuts <christie.barger@gmail.com>
2016-08-28 20:17:39 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -17,6 +17,23 @@
         bot.retrieveSettings();
 
         //Extend the bot here, either by calling another function or here directly.
+        bot.commands.alienCommand = {
+            command: 'alien',
+            rank: 'user',
+            type: 'exact',
+            getAlien: function (chat) {
+                var c = Math.floor(Math.random() * basicBot.chat.aliens.length);
+                return basicBot.chat.aliens[c];
+            },
+                functionality: function (chat, cmd) {
+                            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                            if (!bot.commands.executable(this.rank, chat)) return void (0);
+                            else {
+                    return API.sendChat("oooweeeooo");
+                    }
+                     
+                }
+            }
 
         // You can add more spam words to the bot.
         var spamWords = ['spam1', 'spam2', 'spam3', 'spam4'];
